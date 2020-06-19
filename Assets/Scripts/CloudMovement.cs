8a218d39dd532efd86e56040f7d26daa8d9e97ad
Rishav Nath Pati <patirishavnath@gmail.com>
2020-06-19 15:47:16 +0530

    Location Changes

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -1,18 +1,12 @@
 ﻿using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class CloudMovement : MonoBehaviour
 {
     private Rigidbody rb;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-    }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-
     }
 
     // Update is called once per frame
